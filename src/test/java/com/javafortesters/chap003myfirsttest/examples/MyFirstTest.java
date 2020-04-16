68e82319555dfc760e8663ff22774b85de57f115
Electra Saranti <electra.saranti@gmail.com>
2020-04-16 13:42:52 +0300

    pom.xml edited

---
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -0,0 +1,33 @@
+package com.javafortesters.chap003myfirsttest.examples;
+
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyFirstTest {
+
+    @Test
+    public void canAddTwoPlusTwo(){
+        int answer = 2+2;
+        assertEquals("2+2=4",4,answer);
+    }
+
+    @Test
+    public void canSubtractTwoFromTwo(){
+        int answer = 2-2;
+        assertEquals("2-2=0",0,answer);
+    }
+
+    @Test
+    public void canDivideFourWithTwo(){
+        int answer = 4/2;
+        assertEquals("4/2=2",2,answer);
+    }
+
+    @Test
+    public void canMultiplyTwoWithTwo(){
+        int answer = 2*2;
+        assertEquals("2*2=4",4,answer);
+    }
+}
