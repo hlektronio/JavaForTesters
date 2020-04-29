33966c294ffdd60b02ecb32a5ae6c43329f768d7
Electra Saranti <electra.saranti@gmail.com>
2020-04-29 13:24:20 +0300

    chapter 11 exercise 1

--- src/main/java/com/javafortesters/domainentities/User.java
+++ src/main/java/com/javafortesters/domainentities/User.java
@@ -1,9 +1,12 @@
 package com.javafortesters.domainentities;
 
+import com.javafortesters.domainobject.TestAppEnv;
+
 public class User {
 
     private String username;
     private String password;
+    private TestAppEnv testAppEnv;
 
 
     public User(){
@@ -21,10 +24,17 @@ public class User {
     public User(String username, String password){
         this.username = username;
         this.password = password;
+        this.testAppEnv = new TestAppEnv();
     }
 
     public void setPassword(String password) {
         this.password= password;
     }
+
+    public String getUrl(){
+        return this.testAppEnv.getUrl();
+
+    }
+
 
 }
--- src/test/java/com/javafortesters/chap011exceptions/examples/ExceptionsExampleTest.java
+++ src/test/java/com/javafortesters/chap011exceptions/examples/ExceptionsExampleTest.java
@@ -1,8 +1,6 @@
-package com.javafortesters.chap011exceprions.examples;
+package com.javafortesters.chap011exceptions.examples;
 
 import org.junit.Test;
-
-import java.lang.annotation.Target;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -19,8 +17,14 @@ public class ExceptionsExampleTest {
         catch (NullPointerException e){
             age = 18;
             ageAsString = age.toString();
+            System.out.println("getMessage - "+e.getMessage());
+            System.out.println("getStackTrace - "+e.getStackTrace());
+            System.out.println("printStackTrace");
+            e.printStackTrace();
         }
         String yourAge = "You are "+ageAsString+" years old";
         assertEquals("You are 18 years old",yourAge);
     }
+
+
 }
