f611cd3380002b064bd2888b9ced5dd314df786e
Electra Saranti <electra.saranti@gmail.com>
2020-05-05 11:19:33 +0300

    chapter 13 exercise 1

--- src/test/java/com/javafortesters/chap014junit/examples/TestAllAsserts.java
+++ src/test/java/com/javafortesters/chap014junit/examples/TestAllAsserts.java
@@ -2,6 +2,8 @@ package com.javafortesters.chap014junit.examples;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class TestAllAsserts {
@@ -12,7 +14,22 @@ public class TestAllAsserts {
         int b = 3;
         assertEquals(6,a+b);
         assertFalse("is false",a+b==2);
+        assertFalse("is false",false);
+        assertFalse(false);
         assertTrue("is true",a+b==6);
-        
+        assertTrue("is true",true);
+        assertTrue(true);
+
+        int[] array1 = {3,3,2};
+        int [] array2 = {2,3,3};
+        String i = null;
+
+        assertNotSame(array1,array2);
+        Arrays.sort(array1);
+        assertArrayEquals(array1, array2);
+        assertSame(a,b);
+        assertNotNull(array1);
+        assertNull(i);
+
     }
 }
