ff993c02648c450ac0c7265791d854f606758417
hlektronio <electra.saranti@gmail.com>
2020-05-14 17:59:03 +0300

    chapter 16 exercise 6 solved

---
+++ src/test/java/com/javafortesters/chap017datesandtimes/examples/TestTime.java
@@ -0,0 +1,61 @@
+package com.javafortesters.chap017datesandtimes.examples;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+
+public class TestTime {
+
+
+    @Test
+    public void currentTimeMillis(){
+        long startTime = System.currentTimeMillis();
+
+        for (int i= 0;i<10;i++){
+            System.out.println("Current time "+System.currentTimeMillis());
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Total time "+(endTime-startTime));
+    }
+
+
+    @Test
+    public void timingTest(){
+        long startTime = System.nanoTime();
+
+        for (int i=0; i<10; i++){
+            System.out.println("Current time "+System.nanoTime());
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Total time "+(endTime-startTime));
+    }
+
+    @Test
+    public void createUniqueName() {
+
+        String userId = "user" + System.currentTimeMillis();
+        System.out.println(userId);
+
+
+        for (int i = 0; i<10; i++){
+            String replacement = "" + (char)('A'+i);
+            String intToReplace = String.valueOf(i);
+
+            userId = userId.replace(intToReplace,replacement);
+        }
+        assertTrue(!userId.contains("0"));
+        assertTrue(!userId.contains("1"));
+        assertTrue(!userId.contains("2"));
+        assertTrue(!userId.contains("3"));
+        assertTrue(!userId.contains("4"));
+        assertTrue(!userId.contains("5"));
+        assertTrue(!userId.contains("6"));
+        assertTrue(!userId.contains("7"));
+        assertTrue(!userId.contains("8"));
+        assertTrue(!userId.contains("9"));
+
+
+        System.out.println(userId);
+    }
+}
