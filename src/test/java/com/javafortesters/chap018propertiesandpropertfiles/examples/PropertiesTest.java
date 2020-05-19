7f58e1464bd9ffbf4fc4186f6892e46ef3d91d2c
hlektronio <electra.saranti@gmail.com>
2020-05-19 13:02:28 +0300

    chapter 18 exercise 3 solved

--- src/test/java/com/javafortesters/chap018propertiesandpropertfiles/examples/PropertiesTest.java
+++ src/test/java/com/javafortesters/chap018propertiesandpropertfiles/examples/PropertiesTest.java
@@ -2,6 +2,7 @@ package com.javafortesters.chap018propertiesandpropertfiles.examples;
 
 import org.junit.Test;
 
+import java.io.*;
 import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -40,6 +41,33 @@ public class PropertiesTest {
         System.out.println(lineSeparator);
         System.out.println(fileSeparator);
         System.out.println(tempDir);
+    }
+
+    @Test
+    public void storeLoadPropertiesFile() throws IOException {
+        String temDir = System.getProperty("java.io.tmpdir");
+        String tempResourceFilePath = new File(temDir,
+                "tempFileForPropertiesStoreTest.properties").getAbsolutePath();
+
+        Properties saved = new Properties();
+        saved.setProperty("name", "electra");
+        saved.setProperty("age","41");
+
+        FileOutputStream outputFile = new FileOutputStream(tempResourceFilePath);
+        saved.store(outputFile,"these are my properties");
+        outputFile.close();
+
+        FileReader propertyFileReader = new FileReader(tempResourceFilePath);
+        Properties loaded = new Properties();
+        try{
+            loaded.load(propertyFileReader);
+        }finally {
+            propertyFileReader.close();
+        }
+        assertThat(saved.getProperty("name"),is("electra"));
+        assertThat(saved.getProperty("age"), is("41"));
+
+        new File(tempResourceFilePath).delete();
     }
 
 }
