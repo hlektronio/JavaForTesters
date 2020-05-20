1abc75fb0a133ce39ffd71728273da0cdc30c94d
hlektronio <electra.saranti@gmail.com>
2020-05-20 12:08:58 +0300

    chapter 19 exercise 7 attempt

--- src/test/java/com/javafortesters/chap019workingwithfiles/examples/TestFiles.java
+++ src/test/java/com/javafortesters/chap019workingwithfiles/examples/TestFiles.java
@@ -15,20 +15,20 @@ public class TestFiles {
 
     @Test
     public void createTempFile() throws IOException {
-        File createdFile = File.createTempFile("file",".dmg");
+        File createdFile = File.createTempFile("file", ".dmg");
         System.out.println(createdFile.getAbsolutePath());
-        assertThat(createdFile.exists(),is(true));
+        assertThat(createdFile.exists(), is(true));
         createdFile.delete();
-        assertThat(createdFile.exists(),is(false));
+        assertThat(createdFile.exists(), is(false));
     }
 
     @Test
-    public void displayRoots(){
+    public void displayRoots() {
         File[] roots = File.listRoots();
-        for(File aFile : roots){
+        for (File aFile : roots) {
             System.out.println(aFile.getAbsolutePath());
         }
-        for (int i = 0; i<roots.length; i++){
+        for (int i = 0; i < roots.length; i++) {
             System.out.println(roots[i].getAbsolutePath());
         }
     }
@@ -37,26 +37,26 @@ public class TestFiles {
     public void createTempFileWithCustomCode() throws IOException {
         Long fileNamePart = System.currentTimeMillis();
         File customCodeFile = new File("/var/folders/f1/qw1wk0757zs7kvvklsngtq580000gn/T/",
-                (fileNamePart.toString()+".io"));
+                (fileNamePart.toString() + ".io"));
 
-        File anotherCustomFile = File.createTempFile((fileNamePart.toString()),".io");
+        File anotherCustomFile = File.createTempFile((fileNamePart.toString()), ".io");
 
-        assertThat(customCodeFile.exists(),is(false));
-        assertThat(anotherCustomFile.exists(),is(true));
+        assertThat(customCodeFile.exists(), is(false));
+        assertThat(anotherCustomFile.exists(), is(true));
         System.out.println(customCodeFile.getAbsolutePath());
         System.out.println(anotherCustomFile.getAbsolutePath());
 
         Properties sys = System.getProperties();
         String tempDir = sys.getProperty("java.io.tmpdir");
         System.out.println(tempDir);
-        assertThat(customCodeFile.getAbsolutePath().contains(tempDir),is(true));
-        assertThat(customCodeFile.getAbsolutePath().contains(fileNamePart.toString()),is(true));
-        assertThat(anotherCustomFile.getAbsolutePath().contains(tempDir),is(true));
-        assertThat(anotherCustomFile.getAbsolutePath().contains(fileNamePart.toString()),is(true));
+        assertThat(customCodeFile.getAbsolutePath().contains(tempDir), is(true));
+        assertThat(customCodeFile.getAbsolutePath().contains(fileNamePart.toString()), is(true));
+        assertThat(anotherCustomFile.getAbsolutePath().contains(tempDir), is(true));
+        assertThat(anotherCustomFile.getAbsolutePath().contains(fileNamePart.toString()), is(true));
         customCodeFile.delete();
         anotherCustomFile.delete();
-        assertThat(customCodeFile.exists(),is(false));
-        assertThat(anotherCustomFile.exists(),is(false));
+        assertThat(customCodeFile.exists(), is(false));
+        assertThat(anotherCustomFile.exists(), is(false));
     }
 
     @Test
@@ -64,8 +64,8 @@ public class TestFiles {
         Properties sys = System.getProperties();
         String tempDir = sys.getProperty("java.io.tmpdir");
 
-        Path path = Paths.get(tempDir,"1/2/3/4/5/../../../..");
-        Path anotherPath = Paths.get(tempDir,"1/2/../../1");
+        Path path = Paths.get(tempDir, "1/2/3/4/5/../../../..");
+        Path anotherPath = Paths.get(tempDir, "1/2/../../1");
         System.out.println(path.toFile());
 
         File createdFileOne = path.toFile();
@@ -74,21 +74,21 @@ public class TestFiles {
         System.out.println(createdFileOne.getCanonicalPath());
         System.out.printf(createdFileTwo.getCanonicalPath());
 
-        assertEquals(createdFileOne.getCanonicalPath(),createdFileTwo.getCanonicalPath());
+        assertEquals(createdFileOne.getCanonicalPath(), createdFileTwo.getCanonicalPath());
         createdFileOne.deleteOnExit();
         createdFileTwo.deleteOnExit();
     }
 
     @Test
-    public void checkTempDirectory(){
+    public void checkTempDirectory() {
         File tempDir = new File(System.getProperty("java.io.tmpdir"));
-        assertThat(tempDir.isFile(),is(false));
-        assertThat(tempDir.isDirectory(),is(true));
+        assertThat(tempDir.isFile(), is(false));
+        assertThat(tempDir.isDirectory(), is(true));
     }
 
     @Test
     public void printWriterAndAppend() throws IOException {
-        File tempFile = new File(System.getProperty("java.io.tmpdir"),"text.txt");
+        File tempFile = new File(System.getProperty("java.io.tmpdir"), "text.txt");
         System.out.println(tempFile.getCanonicalPath());
 
         FileWriter writer = new FileWriter(tempFile);
@@ -100,14 +100,51 @@ public class TestFiles {
         print.println(textToWrite);
         print.close();
 
-        writer = new FileWriter(tempFile,true);
+        writer = new FileWriter(tempFile, true);
         buffer = new BufferedWriter(writer);
         print = new PrintWriter(buffer);
 
         print.println("===========================");
         print.close();
         tempFile.deleteOnExit();
-
     }
 
-}
+    @Test
+    public void calculateFileLength() throws IOException {
+        File aFile = new File(System.getProperty("java.io.tmpdir"),"aFile");
+        System.out.println(aFile.length());
+        assertEquals(aFile.length(),0);
+
+        FileWriter writer = new FileWriter(aFile);
+        BufferedWriter buffer = new BufferedWriter(writer);
+        PrintWriter print = new PrintWriter(buffer);
+
+        String textToWrite = "it's written in text now";
+
+
+        print.println(textToWrite);
+        print.close();
+
+        BufferedReader reader = new BufferedReader(new FileReader(aFile));
+        try{
+            String line;
+            while((line = reader.readLine())!=null){
+                System.out.println(line);
+            }
+        }finally{
+                reader.close();
+            }
+
+        String ignoredChars = System.lineSeparator();
+        int ignored = ignoredChars.length();
+        assertEquals(aFile.length(),textToWrite.length()+ignored);
+        System.out.println(ignoredChars.length());;
+        System.out.println(aFile.getUsableSpace());
+        System.out.println(aFile.getTotalSpace());
+        System.out.println(aFile.getFreeSpace());
+
+        aFile.delete();
+        }
+    }
+
+
